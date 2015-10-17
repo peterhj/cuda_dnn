20d47aa7a618a1cb1d7dc547d1e84b5fffe1ccc6
Peter Jin <peterhaijin@gmail.com>
2015-10-17 12:48:13 -0700

    Implement more wrappers. Currently supports convolution, softmax.

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,6 +1,6 @@
 use cuda::ffi::runtime::{cudaStream_t};
 
-use libc::{c_char, size_t};
+use libc::{c_void, c_char, c_int, c_float, size_t};
 
 #[repr(C)]
 struct cudnnContext;
@@ -22,11 +22,38 @@ pub enum cudnnStatus_t {
   LicenseError    = 10,
 }
 
+impl Default for cudnnStatus_t {
+  fn default() -> cudnnStatus_t {
+    cudnnStatus_t::Success
+  }
+}
+
+#[repr(C)]
+struct cudnnTensorStruct;
+pub type cudnnTensorDescriptor_t = *mut cudnnTensorStruct;
+
+#[repr(C)]
+struct cudnnConvolutionStruct;
+pub type cudnnConvolutionDescriptor_t = *mut cudnnConvolutionStruct;
+
+#[repr(C)]
+struct cudnnPoolingStruct;
+pub type cudnnPoolingDescriptor_t = *mut cudnnPoolingStruct;
+
+#[repr(C)]
+struct cudnnFilterStruct;
+pub type cudnnFilterDescriptor_t = *mut cudnnFilterStruct;
+
+#[repr(C)]
+struct cudnnLRNStruct;
+pub type cudnnLRNDescriptor_t = *mut cudnnLRNStruct;
+
 #[derive(Clone, Copy)]
 #[repr(C)]
 pub enum cudnnDataType_t {
   Float   = 0,
   Double  = 1,
+  Half    = 2,
 }
 
 #[derive(Clone, Copy)]
@@ -62,13 +89,92 @@ pub enum cudnnConvolutionFwdPreference_t {
   SpecifyWorkspaceLimit = 2,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 #[repr(C)]
 pub enum cudnnConvolutionFwdAlgo_t {
   ImplicitGemm        = 0,
   ImplicitPrecompGemm = 1,
   Gemm                = 2,
   Direct              = 3,
+  Fft                 = 4,
+}
+
+impl Default for cudnnConvolutionFwdAlgo_t {
+  fn default() -> cudnnConvolutionFwdAlgo_t {
+    cudnnConvolutionFwdAlgo_t::ImplicitGemm
+  }
+}
+
+#[derive(Clone, Copy, Default, Debug)]
+#[repr(C)]
+pub struct cudnnConvolutionFwdAlgoPerf_t {
+  pub algo:   cudnnConvolutionFwdAlgo_t,
+  pub status: cudnnStatus_t,
+  pub time:   c_float,
+  pub memory: size_t,
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub enum cudnnConvolutionBwdFilterPreference_t {
+  NoWorkspace           = 0,
+  PreferFastest         = 1,
+  SpecifyWorkspaceLimit = 2,
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub enum cudnnConvolutionBwdFilterAlgo_t {
+  NonDeterministic          = 0,
+  Deterministic             = 1,
+  Fft                       = 2,
+  NonDeterministicWorkspace = 3,
+}
+
+impl Default for cudnnConvolutionBwdFilterAlgo_t {
+  fn default() -> cudnnConvolutionBwdFilterAlgo_t {
+    cudnnConvolutionBwdFilterAlgo_t::NonDeterministic
+  }
+}
+
+#[derive(Clone, Copy, Default)]
+#[repr(C)]
+pub struct cudnnConvolutionBwdFilterAlgoPerf_t {
+  pub algo:   cudnnConvolutionBwdFilterAlgo_t,
+  pub status: cudnnStatus_t,
+  pub time:   c_float,
+  pub memory: size_t,
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub enum cudnnConvolutionBwdDataPreference_t {
+  NoWorkspace           = 0,
+  PreferFastest         = 1,
+  SpecifyWorkspaceLimit = 2,
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub enum cudnnConvolutionBwdDataAlgo_t {
+  NonDeterministic  = 0,
+  Deterministic     = 1,
+  Fft               = 2,
+}
+
+impl Default for cudnnConvolutionBwdDataAlgo_t {
+  fn default() -> cudnnConvolutionBwdDataAlgo_t {
+    cudnnConvolutionBwdDataAlgo_t::NonDeterministic
+  }
+}
+
+#[derive(Clone, Copy, Default)]
+#[repr(C)]
+pub struct cudnnConvolutionBwdDataAlgoPerf_t {
+  pub algo:   cudnnConvolutionBwdDataAlgo_t,
+  pub status: cudnnStatus_t,
+  pub time:   c_float,
+  pub memory: size_t,
 }
 
 #[derive(Clone, Copy)]
@@ -76,6 +182,7 @@ pub enum cudnnConvolutionFwdAlgo_t {
 pub enum cudnnSoftmaxAlgorithm_t {
   Fast      = 0,
   Accurate  = 1,
+  Log       = 2,
 }
 
 #[derive(Clone, Copy)]
@@ -111,6 +218,181 @@ extern "C" {
   pub fn cudnnDestroy(handle: cudnnHandle_t) -> cudnnStatus_t;
   pub fn cudnnSetStream(handle: cudnnHandle_t, stream: cudaStream_t) -> cudnnStatus_t;
   pub fn cudnnGetStream(handle: cudnnHandle_t, stream: *mut cudaStream_t) -> cudnnStatus_t;
+
+  pub fn cudnnCreateTensorDescriptor(tensor_desc: *mut cudnnTensorDescriptor_t) -> cudnnStatus_t;
+  pub fn cudnnDestroyTensorDescriptor(tensor_desc: cudnnTensorDescriptor_t) -> cudnnStatus_t;
+  pub fn cudnnSetTensor4dDescriptor(
+      tensor_desc: cudnnTensorDescriptor_t,
+      format: cudnnTensorFormat_t,
+      data_ty: cudnnDataType_t,
+      n: c_int,
+      c: c_int,
+      h: c_int,
+      w: c_int,
+  ) -> cudnnStatus_t;
+  pub fn cudnnCreateFilterDescriptor(filter_desc: *mut cudnnFilterDescriptor_t) -> cudnnStatus_t;
+  pub fn cudnnDestroyFilterDescriptor(filter_desc: cudnnFilterDescriptor_t) -> cudnnStatus_t;
+  pub fn cudnnSetFilter4dDescriptor(
+      filter_desc: cudnnFilterDescriptor_t,
+      data_ty: cudnnDataType_t,
+      k: c_int,
+      c: c_int,
+      h: c_int,
+      w: c_int,
+  ) -> cudnnStatus_t;
+  pub fn cudnnCreateConvolutionDescriptor(conv_desc: *mut cudnnConvolutionDescriptor_t) -> cudnnStatus_t;
+  pub fn cudnnDestroyConvolutionDescriptor(conv_desc: cudnnConvolutionDescriptor_t) -> cudnnStatus_t;
+  pub fn cudnnSetConvolution2dDescriptor(
+      conv_desc: cudnnConvolutionDescriptor_t,
+      pad_h: c_int,
+      pad_w: c_int,
+      u: c_int,
+      v: c_int,
+      upscalex: c_int,
+      upscaley: c_int,
+      conv_mode: cudnnConvolutionMode_t,
+  ) -> cudnnStatus_t;
+
+  // TODO
+
+  pub fn cudnnFindConvolutionForwardAlgorithm(
+      handle: cudnnHandle_t,
+      src_desc: cudnnTensorDescriptor_t,
+      filter_desc: cudnnFilterDescriptor_t,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      dest_desc: cudnnTensorDescriptor_t,
+      requested_algo_count: c_int,
+      returned_algo_count: *mut c_int,
+      perf_results: *mut cudnnConvolutionFwdAlgoPerf_t,
+  ) -> cudnnStatus_t;
+  /*pub fn cudnnGetConvolutionForwardAlgorithm(
+      handle: cudnnHandle_t,
+      src_desc: cudnnTensorDescriptor_t,
+      filter_desc: cudnnFilterDescriptor_t,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      dest_desc: cudnnTensorDescriptor_t,
+      preference: cudnnConvolutionFwdPreference_t,
+      memory_limit: size_t,
+      algo: *mut cudnnConvolutionFwdAlgo_t,
+  ) -> cudnnStatus_t;*/
+  pub fn cudnnFindConvolutionBackwardFilterAlgorithm(
+      handle: cudnnHandle_t,
+      src_desc: cudnnTensorDescriptor_t,
+      diff_desc: cudnnTensorDescriptor_t,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      grad_desc: cudnnFilterDescriptor_t,
+      requested_algo_count: c_int,
+      returned_algo_count: *mut c_int,
+      perf_results: *mut cudnnConvolutionBwdFilterAlgoPerf_t,
+  ) -> cudnnStatus_t;
+  pub fn cudnnFindConvolutionBackwardDataAlgorithm(
+      handle: cudnnHandle_t,
+      filter_desc: cudnnFilterDescriptor_t,
+      diff_desc: cudnnTensorDescriptor_t,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      grad_desc: cudnnTensorDescriptor_t,
+      requested_algo_count: c_int,
+      returned_algo_count: *mut c_int,
+      perf_results: *mut cudnnConvolutionBwdDataAlgoPerf_t,
+  ) -> cudnnStatus_t;
+  pub fn cudnnGetConvolutionBackwardFilterWorkspaceSize(
+      handle: cudnnHandle_t,
+      src_desc: cudnnTensorDescriptor_t,
+      diff_desc: cudnnTensorDescriptor_t,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      grad_desc: cudnnFilterDescriptor_t,
+      algo: cudnnConvolutionBwdFilterAlgo_t,
+      size_in_bytes: *mut size_t,
+  ) -> cudnnStatus_t;
+  pub fn cudnnGetConvolutionBackwardDataWorkspaceSize(
+      handle: cudnnHandle_t,
+      filter_desc: cudnnFilterDescriptor_t,
+      diff_desc: cudnnTensorDescriptor_t,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      grad_desc: cudnnTensorDescriptor_t,
+      algo: cudnnConvolutionBwdDataAlgo_t,
+      size_in_bytes: *mut size_t,
+  ) -> cudnnStatus_t;
+
+  pub fn cudnnConvolutionForward(
+      handle: cudnnHandle_t,
+      alpha: *const c_void,
+      src_desc: cudnnTensorDescriptor_t,
+      src_data: *const c_void,
+      filter_desc: cudnnFilterDescriptor_t,
+      filter_data: *const c_void,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      algo: cudnnConvolutionFwdAlgo_t,
+      work_space: *mut c_void,
+      work_space_size_in_bytes: size_t,
+      beta: *const c_void,
+      dst_desc: cudnnTensorDescriptor_t,
+      dst_data: *mut c_void,
+  ) -> cudnnStatus_t;
+  pub fn cudnnConvolutionBackwardBias(
+      handle: cudnnHandle_t,
+      alpha: *const c_void,
+      src_desc: cudnnTensorDescriptor_t,
+      src_data: *const c_void,
+      beta: *const c_void,
+      dst_desc: cudnnTensorDescriptor_t,
+      dst_data: *mut c_void,
+  ) -> cudnnStatus_t;
+  pub fn cudnnConvolutionBackwardFilter_v3(
+      handle: cudnnHandle_t,
+      alpha: *const c_void,
+      src_desc: cudnnTensorDescriptor_t,
+      src_data: *const c_void,
+      diff_desc: cudnnTensorDescriptor_t,
+      diff_data: *const c_void,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      algo: cudnnConvolutionBwdFilterAlgo_t,
+      work_space: *mut c_void,
+      work_space_size_in_bytes: size_t,
+      beta: *const c_void,
+      grad_desc: cudnnFilterDescriptor_t,
+      grad_data: *mut c_void,
+  ) -> cudnnStatus_t;
+  pub fn cudnnConvolutionBackwardData_v3(
+      handle: cudnnHandle_t,
+      alpha: *const c_void,
+      filter_desc: cudnnFilterDescriptor_t,
+      filter_data: *const c_void,
+      diff_desc: cudnnTensorDescriptor_t,
+      diff_data: *const c_void,
+      conv_desc: cudnnConvolutionDescriptor_t,
+      algo: cudnnConvolutionBwdDataAlgo_t,
+      work_space: *mut c_void,
+      work_space_size_in_bytes: size_t,
+      beta: *const c_void,
+      grad_desc: cudnnTensorDescriptor_t,
+      grad_data: *mut c_void,
+  ) -> cudnnStatus_t;
+
+  pub fn cudnnSoftmaxForward(
+      handle: cudnnHandle_t,
+      algorithm: cudnnSoftmaxAlgorithm_t,
+      mode: cudnnSoftmaxMode_t,
+      alpha: *const c_void,
+      src_desc: cudnnTensorDescriptor_t,
+      src_data: *const c_void,
+      beta: *const c_void,
+      dst_desc: cudnnTensorDescriptor_t,
+      dst_data: *mut c_void,
+  ) -> cudnnStatus_t;
+  pub fn cudnnSoftmaxBackward(
+      handle: cudnnHandle_t,
+      algorithm: cudnnSoftmaxAlgorithm_t,
+      mode: cudnnSoftmaxMode_t,
+      alpha: *const c_void,
+      src_desc: cudnnTensorDescriptor_t,
+      src_data: *const c_void,
+      src_diff_desc: cudnnTensorDescriptor_t,
+      src_diff_data: *const c_void,
+      beta: *const c_void,
+      dst_diff_desc: cudnnTensorDescriptor_t,
+      dst_diff_data: *mut c_void,
+  ) -> cudnnStatus_t;
 
   // TODO
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,406 @@
+use ffi::*;
+
+use cuda::runtime::{CudaStream};
+
+use libc::{c_void, c_int, size_t};
+use std::marker::{PhantomData};
+use std::ptr::{null_mut};
+
 extern crate cuda;
 extern crate libc;
 
 pub mod ffi;
+
+pub trait CudnnDataTypeExt {
+  fn data_ty() -> cudnnDataType_t;
+}
+
+impl CudnnDataTypeExt for f32 {
+  fn data_ty() -> cudnnDataType_t {
+    cudnnDataType_t::Float
+  }
+}
+
+impl CudnnDataTypeExt for f64 {
+  fn data_ty() -> cudnnDataType_t {
+    cudnnDataType_t::Double
+  }
+}
+
+pub type CudnnResult<T> = Result<T, cudnnStatus_t>;
+
+fn new_result<T>(value: T, status: cudnnStatus_t) -> CudnnResult<T> {
+  match status {
+    cudnnStatus_t::Success => Ok(value),
+    x => Err(x),
+  }
+}
+
+pub struct CudnnHandle {
+  ptr: cudnnHandle_t,
+}
+
+impl CudnnHandle {
+  pub fn create() -> CudnnResult<CudnnHandle> {
+    let mut inner: cudnnHandle_t = null_mut();
+    let status = unsafe { cudnnCreate(&mut inner as *mut cudnnHandle_t) };
+    new_result(CudnnHandle{ptr: inner}, status)
+  }
+
+  pub fn set_stream(&self, stream: &CudaStream) -> CudnnResult<()> {
+    new_result((), unsafe { cudnnSetStream(self.ptr, stream.ptr) })
+  }
+}
+
+impl Drop for CudnnHandle {
+  fn drop(&mut self) {
+    match unsafe { cudnnDestroy(self.ptr) } {
+      cudnnStatus_t::Success => {}
+      x => panic!("PANIC: failed to destroy cudnn handle: {:?}", x),
+    }
+  }
+}
+
+pub struct CudnnTensorDesc<T> where T: CudnnDataTypeExt {
+  ptr: cudnnTensorDescriptor_t,
+  _marker: PhantomData<T>,
+}
+
+impl<T> CudnnTensorDesc<T> where T: CudnnDataTypeExt {
+  pub fn create_4d(width: usize, height: usize, channels: usize, num: usize) -> CudnnResult<CudnnTensorDesc<f32>> {
+    let mut inner: cudnnTensorDescriptor_t = null_mut();
+    let status = unsafe { cudnnCreateTensorDescriptor(&mut inner as *mut _) };
+    new_result(CudnnTensorDesc{ptr: inner, _marker: PhantomData}, status)
+      .and_then(|desc| {
+        let status = unsafe { cudnnSetTensor4dDescriptor(
+            desc.ptr,
+            // FIXME(20151001): may want to specify data layout.
+            cudnnTensorFormat_t::RowMajorNCHW,
+            T::data_ty(),
+            num as c_int, channels as c_int, height as c_int, width as c_int,
+        ) };
+        new_result(desc, status)
+      })
+  }
+}
+
+impl<T> Drop for CudnnTensorDesc<T> where T: CudnnDataTypeExt {
+  fn drop(&mut self) {
+    match unsafe { cudnnDestroyTensorDescriptor(self.ptr) } {
+      cudnnStatus_t::Success => {}
+      x => panic!("PANIC: failed to destroy cudnn tensor desc: {:?}", x),
+    }
+  }
+}
+
+pub struct CudnnFilterDesc<T> where T: CudnnDataTypeExt {
+  ptr: cudnnFilterDescriptor_t,
+  _marker: PhantomData<T>,
+}
+
+impl<T> CudnnFilterDesc<T> where T: CudnnDataTypeExt {
+  pub fn create_4d(width: usize, height: usize, in_channels: usize, out_channels: usize) -> CudnnResult<CudnnFilterDesc<f32>> {
+    let mut inner: cudnnFilterDescriptor_t = null_mut();
+    let status = unsafe { cudnnCreateFilterDescriptor(&mut inner as *mut _) };
+    new_result(CudnnFilterDesc{ptr: inner, _marker: PhantomData}, status)
+      .and_then(|desc| {
+        let status = unsafe { cudnnSetFilter4dDescriptor(
+            desc.ptr,
+            T::data_ty(),
+            out_channels as c_int, in_channels as c_int, height as c_int, width as c_int,
+        ) };
+        new_result(desc, status)
+      })
+  }
+}
+
+impl<T> Drop for CudnnFilterDesc<T> where T: CudnnDataTypeExt {
+  fn drop(&mut self) {
+    match unsafe { cudnnDestroyFilterDescriptor(self.ptr) } {
+      cudnnStatus_t::Success => {}
+      x => panic!("PANIC: failed to destroy cudnn filter desc: {:?}", x),
+    }
+  }
+}
+
+pub struct CudnnConvDesc {
+  ptr: cudnnConvolutionDescriptor_t,
+}
+
+impl CudnnConvDesc {
+  pub fn create_2d_symmetric(stride: usize, pad: usize) -> CudnnResult<CudnnConvDesc> {
+    CudnnConvDesc::create_2d(stride, stride, pad, pad)
+  }
+
+  pub fn create_2d(stride_w: usize, stride_h: usize, pad_w: usize, pad_h: usize) -> CudnnResult<CudnnConvDesc> {
+    let mut inner: cudnnConvolutionDescriptor_t = null_mut();
+    let status = unsafe { cudnnCreateConvolutionDescriptor(&mut inner as *mut _) };
+    new_result(CudnnConvDesc{ptr: inner}, status)
+      .and_then(|desc| {
+        let status = unsafe { cudnnSetConvolution2dDescriptor(
+            desc.ptr,
+            // XXX(20151001): be careful about the argument order.
+            pad_h as c_int, pad_w as c_int,
+            stride_w as c_int, stride_h as c_int,
+            1, 1,
+            // FIXME(20151001): may want to specify this.
+            cudnnConvolutionMode_t::CrossCorrelation,
+        ) };
+        new_result(desc, status)
+      })
+  }
+}
+
+impl Drop for CudnnConvDesc {
+  fn drop(&mut self) {
+    match unsafe { cudnnDestroyConvolutionDescriptor(self.ptr) } {
+      cudnnStatus_t::Success => {}
+      x => panic!("PANIC: failed to destroy cudnn conv desc: {:?}", x),
+    }
+  }
+}
+
+pub struct CudnnConvFwdDesc {
+  pub algo:         cudnnConvolutionFwdAlgo_t,
+  pub work_size:    usize,
+  pub time_ms:      f32,
+  pub src_desc:     CudnnTensorDesc<f32>,
+  pub filter_desc:  CudnnFilterDesc<f32>,
+  pub conv_desc:    CudnnConvDesc,
+  pub dst_desc:     CudnnTensorDesc<f32>,
+}
+
+impl CudnnConvFwdDesc {
+  pub fn create_fastest(src_desc: CudnnTensorDesc<f32>, filter_desc: CudnnFilterDesc<f32>, conv_desc: CudnnConvDesc, dst_desc: CudnnTensorDesc<f32>, handle: &CudnnHandle) -> CudnnResult<CudnnConvFwdDesc> {
+    let mut count: c_int = 0;
+    let mut inner: cudnnConvolutionFwdAlgoPerf_t = Default::default();
+    let status = unsafe { cudnnFindConvolutionForwardAlgorithm(
+        handle.ptr,
+        src_desc.ptr,
+        filter_desc.ptr,
+        conv_desc.ptr,
+        dst_desc.ptr,
+        1, &mut count as *mut _,
+        &mut inner as *mut _,
+    ) };
+    //println!("DEBUG: perf: {:?}", inner);
+    new_result(CudnnConvFwdDesc{
+      algo: inner.algo, work_size: inner.memory as usize, time_ms: inner.time,
+      src_desc: src_desc,
+      filter_desc: filter_desc,
+      conv_desc: conv_desc,
+      dst_desc: dst_desc,
+    }, status)
+  }
+
+  pub unsafe fn forward(&self, in_act: *const f32, filter: *const f32, out_act: *mut f32, work_space: *mut f32, handle: &CudnnHandle) -> CudnnResult<()> {
+    let alpha: f32 = 1.0;
+    let beta: f32 = 0.0;
+    let status = unsafe { cudnnConvolutionForward(
+        handle.ptr,
+        &alpha as *const f32 as *const c_void,
+        self.src_desc.ptr,
+        in_act as *const c_void,
+        self.filter_desc.ptr,
+        filter as *const c_void,
+        self.conv_desc.ptr,
+        self.algo,
+        work_space as *mut c_void,
+        self.work_size as size_t,
+        &beta as *const f32 as *const c_void,
+        self.dst_desc.ptr,
+        out_act as *mut c_void,
+    ) };
+    new_result((), status)
+  }
+}
+
+pub struct CudnnConvBwdFilterDesc {
+  pub algo:       cudnnConvolutionBwdFilterAlgo_t,
+  pub work_size:  usize,
+  pub time_ms:    f32,
+  pub src_desc:         CudnnTensorDesc<f32>,
+  pub diff_desc:        CudnnTensorDesc<f32>,
+  pub conv_desc:        CudnnConvDesc,
+  pub grad_filter_desc: CudnnFilterDesc<f32>,
+  pub grad_bias_desc:   CudnnTensorDesc<f32>,
+}
+
+impl CudnnConvBwdFilterDesc {
+  pub fn create_fastest(src_desc: CudnnTensorDesc<f32>, diff_desc: CudnnTensorDesc<f32>, conv_desc: CudnnConvDesc, grad_filter_desc: CudnnFilterDesc<f32>, grad_bias_desc: CudnnTensorDesc<f32>, handle: &CudnnHandle) -> CudnnResult<CudnnConvBwdFilterDesc> {
+    let mut count: c_int = 0;
+    let mut inner: cudnnConvolutionBwdFilterAlgoPerf_t = Default::default();
+    let status = unsafe { cudnnFindConvolutionBackwardFilterAlgorithm(
+        handle.ptr,
+        src_desc.ptr,
+        diff_desc.ptr,
+        conv_desc.ptr,
+        grad_filter_desc.ptr,
+        1, &mut count as *mut _,
+        &mut inner as *mut _,
+    ) };
+    //println!("DEBUG: perf: {:?}", inner);
+    new_result(CudnnConvBwdFilterDesc{
+      algo: inner.algo, work_size: inner.memory as usize, time_ms: inner.time,
+      src_desc: src_desc,
+      diff_desc: diff_desc,
+      conv_desc: conv_desc,
+      grad_filter_desc: grad_filter_desc,
+      grad_bias_desc: grad_bias_desc,
+    }, status)
+  }
+
+  pub unsafe fn backward_filter(&self, in_act: *const f32, out_delta: *const f32, grad_filter_accum: *mut f32, work_space: *mut f32, handle: &CudnnHandle) -> CudnnResult<()> {
+    let alpha: f32 = 1.0;
+    let beta: f32 = 1.0;
+    let status = unsafe { cudnnConvolutionBackwardFilter_v3(
+        handle.ptr,
+        &alpha as *const f32 as *const c_void,
+        self.src_desc.ptr,
+        in_act as *const c_void,
+        self.diff_desc.ptr,
+        out_delta as *const c_void,
+        self.conv_desc.ptr,
+        self.algo,
+        work_space as *mut c_void,
+        self.work_size as size_t,
+        &beta as *const f32 as *const c_void,
+        self.grad_filter_desc.ptr,
+        grad_filter_accum as *mut c_void,
+    ) };
+    new_result((), status)
+  }
+
+  pub unsafe fn backward_bias(&self, out_delta: *const f32, grad_bias_accum: *mut f32, handle: &CudnnHandle) -> CudnnResult<()> {
+    let alpha: f32 = 1.0;
+    let beta: f32 = 1.0;
+    let status = unsafe { cudnnConvolutionBackwardBias(
+        handle.ptr,
+        &alpha as *const f32 as *const c_void,
+        self.diff_desc.ptr,
+        out_delta as *const c_void,
+        &beta as *const f32 as *const c_void,
+        self.grad_bias_desc.ptr,
+        grad_bias_accum as *mut c_void,
+    ) };
+    new_result((), status)
+  }
+}
+
+pub struct CudnnConvBwdDataDesc {
+  pub algo:         cudnnConvolutionBwdDataAlgo_t,
+  pub work_size:    usize,
+  pub time_ms:      f32,
+  pub filter_desc:  CudnnFilterDesc<f32>,
+  pub diff_desc:    CudnnTensorDesc<f32>,
+  pub conv_desc:    CudnnConvDesc,
+  pub grad_desc:    CudnnTensorDesc<f32>,
+}
+
+impl CudnnConvBwdDataDesc {
+  pub fn create_fastest(filter_desc: CudnnFilterDesc<f32>, diff_desc: CudnnTensorDesc<f32>, conv_desc: CudnnConvDesc, grad_desc: CudnnTensorDesc<f32>, handle: &CudnnHandle) -> CudnnResult<CudnnConvBwdDataDesc> {
+    let mut count: c_int = 0;
+    let mut inner: cudnnConvolutionBwdDataAlgoPerf_t = Default::default();
+    let status = unsafe { cudnnFindConvolutionBackwardDataAlgorithm(
+        handle.ptr,
+        filter_desc.ptr,
+        diff_desc.ptr,
+        conv_desc.ptr,
+        grad_desc.ptr,
+        1, &mut count as *mut _,
+        &mut inner as *mut _,
+    ) };
+    //println!("DEBUG: perf: {:?}", inner);
+    new_result(CudnnConvBwdDataDesc{
+      algo: inner.algo, work_size: inner.memory as usize, time_ms: inner.time,
+      filter_desc: filter_desc,
+      diff_desc: diff_desc,
+      conv_desc: conv_desc,
+      grad_desc: grad_desc,
+    }, status)
+  }
+
+  pub unsafe fn backward_data(&self, filter: *const f32, out_delta: *const f32, in_delta: *mut f32, work_space: *mut f32, handle: &CudnnHandle) -> CudnnResult<()> {
+    let alpha: f32 = 1.0;
+    let beta: f32 = 0.0;
+    let status = unsafe { cudnnConvolutionBackwardData_v3(
+        handle.ptr,
+        &alpha as *const f32 as *const c_void,
+        self.filter_desc.ptr,
+        filter as *const c_void,
+        self.diff_desc.ptr,
+        out_delta as *const c_void,
+        self.conv_desc.ptr,
+        self.algo,
+        work_space as *mut c_void,
+        self.work_size as size_t,
+        &beta as *const f32 as *const c_void,
+        self.grad_desc.ptr,
+        in_delta as *mut c_void,
+    ) };
+    new_result((), status)
+  }
+}
+
+/*pub fn find_fastest_conv_fwd_alg(src_desc: &CudnnTensorDesc<f32>, filter_desc: &CudnnFilterDesc<f32>, conv_desc: &CudnnConvDesc, dst_desc: &CudnnTensorDesc<f32>,  handle: &CudnnHandle) -> CudnnResult<cudnnConvolutionFwdAlgo_t> {
+  let mut count: c_int = 0;
+  let mut alg: cudnnConvolutionFwdAlgo_t = Default::default();
+  let status = unsafe { cudnnGetConvolutionForwardAlgorithm(
+      handle.ptr,
+      src_desc.ptr,
+      filter_desc.ptr,
+      conv_desc.ptr,
+      dst_desc.ptr,
+      cudnnConvolutionFwdPreference_t::PreferFastest,
+      -1usize as size_t,
+      &mut alg as *mut _,
+  ) };
+  println!("DEBUG: fastest alg: {:?}", alg);
+  new_result(alg, status)
+}*/
+
+pub struct CudnnSoftmaxDesc {
+  src_desc:       CudnnTensorDesc<f32>,
+  src_diff_desc:  CudnnTensorDesc<f32>,
+  dst_desc:       CudnnTensorDesc<f32>,
+  dst_diff_desc:  CudnnTensorDesc<f32>,
+}
+
+impl CudnnSoftmaxDesc {
+  pub unsafe fn forward(&self, in_act: *const f32, out_act: *mut f32, handle: &CudnnHandle) -> CudnnResult<()> {
+    let alpha: f32 = 1.0;
+    let beta: f32 = 0.0;
+    let status = unsafe { cudnnSoftmaxForward(
+        handle.ptr,
+        cudnnSoftmaxAlgorithm_t::Accurate,
+        cudnnSoftmaxMode_t::Instance,
+        &alpha as *const f32 as *const c_void,
+        self.src_desc.ptr,
+        in_act as *const c_void,
+        &beta as *const f32 as *const c_void,
+        self.dst_desc.ptr,
+        out_act as *mut c_void,
+    ) };
+    new_result((), status)
+  }
+
+  // FIXME(20151016): the notation here is really unclear.
+  /*pub unsafe fn backward(&self, out_act: *const f32, out_delta: *const f32, in_delta: *mut f32, handle: &CudnnHandle) -> CudnnResult<()> {
+    let alpha: f32 = 1.0;
+    let beta: f32 = 0.0;
+    let status = unsafe { cudnnSoftmaxForward(
+        handle.ptr,
+        cudnnSoftmaxAlgorithm_t::Accurate,
+        cudnnSoftmaxMode_t::Instance,
+        &alpha as *const f32 as *const c_void,
+        self.dst_desc.ptr,
+        out_act as *const c_void,
+        self.src_diff_desc.ptr,
+        out_delta as *const c_void,
+        &beta as *const f32 as *const c_void,
+        self.dst_diff_desc.ptr,
+        in_act as *mut c_void,
+    ) };
+    new_result((), status)
+  }*/
+}
