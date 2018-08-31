ad10332e0c90f5ec3ac0c2ac7c94658f4074d1cc
Peter Jin <peterhaijin@gmail.com>
2018-08-31 14:01:47 -0700

    Use new cuda FFI type paths.

--- src/ffi.rs
+++ src/ffi.rs
@@ -2,5 +2,5 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 
-use cuda::ffi::runtime::{cudaStream_t};
+use cuda::ffi::driver_types::{cudaStream_t};
 include!(concat!(env!("OUT_DIR"), "/cudnn_bind.rs"));
